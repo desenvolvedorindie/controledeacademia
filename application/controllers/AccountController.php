72f5d07781569ad6b7c127f1f303cf7643c4e523
Welsiton Ferreira <wfcreations@gmail.com>
2013-04-16 03:11:11 -0300

    Commit Inicial do projeto

---
+++ application/controllers/AccountController.php
@@ -0,0 +1,32 @@
+<?php
+
+class AccountController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        /* Initialize action controller here */
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function loginAction()
+    {
+        
+    }
+
+    public function logoutAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
+
+
